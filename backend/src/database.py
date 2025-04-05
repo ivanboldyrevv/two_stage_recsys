d0d89e2918b8fccf78d02e762d41fbb1a6078381
ivan <1ibah1ivan1@gmail.com>
2025-04-05 17:27:32 +0400

    added database interface

---
+++ backend/src/database.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine, orm
+from sqlalchemy.orm import Session, declarative_base
+
+from contextlib import contextmanager
+from typing import Iterator
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+Base = declarative_base()
+
+
+class Database:
+    def __init__(self, db_uri: str) -> None:
+        self._engine = create_engine(db_uri, echo=True)
+        self._session_factory = orm.scoped_session(
+            orm.sessionmaker(
+                autocommit=False,
+                autoflush=False,
+                bind=self._engine,
+            ),
+        )
+
+    def create_database(self) -> None:
+        Base.metadata.create_all(self._engine)
+
+    @contextmanager
+    def session(self) -> Iterator:
+        session: Session = self._session_factory()
+        try:
+            yield session
+        except Exception:
+            logger.exception("Session rollback because of exception")
+            session.rollback()
+            raise
+        finally:
+            session.close()
