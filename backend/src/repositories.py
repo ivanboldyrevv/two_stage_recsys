fb2f73ccedad71f0c58f03aad6b9293041f6284b
ivan <1ibah1ivan1@gmail.com>
2025-04-07 18:24:00 +0400

    added Transaction Repository

--- backend/src/repositories.py
+++ backend/src/repositories.py
@@ -1,11 +1,13 @@
-from uuid import UUID
+from uuid import UUID, uuid4
+from datetime import datetime
+
 from contextlib import AbstractContextManager
-from typing import Callable, Iterator, TypeVar, Generic, Type, Iterable, Optional
+from typing import Callable, TypeVar, Generic, Type, List, Optional
 
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
-from models import Customer, Article
+from models import Customer, Article, Transaction
 
 T = TypeVar("T")
 
@@ -15,7 +17,7 @@ class Repository(Generic[T]):
         self.session_factory = session_factory
         self.model = model
 
-    def get_all(self) -> Iterator[T]:
+    def get_all(self) -> List[T]:
         with self.session_factory() as session:
             return session.query(self.model).all()
 
@@ -40,7 +42,7 @@ class ArticleRepository(Repository[Article]):
                      limit: int,
                      offset: int,
                      type_name: Optional[str] = None,
-                     group_name: Optional[str] = None) -> Iterable[Article]:
+                     group_name: Optional[str] = None) -> List[Article]:
         with self.session_factory() as session:
             query = select(Article).limit(limit).offset(offset)
 
@@ -57,6 +59,42 @@ class ArticleRepository(Repository[Article]):
             return articles
 
 
+class TransactionRepository(Repository[Transaction]):
+    def __init__(self, session_factory: Callable[..., AbstractContextManager[Session]]) -> None:
+        super().__init__(session_factory, Transaction)
+
+    def insert_transaction(self, customer_uuid: UUID, article_uuid: UUID) -> Transaction:
+        with self.session_factory() as session:
+            session.add(Transaction(
+                transaction_uuid=uuid4(),
+                t_dat=datetime.now().strftime("%Y-%m-%d"),
+                customer_uuid=customer_uuid,
+                article_uuid=article_uuid
+            ))
+            session.commit()
+
+            entity = (session
+                      .query(Transaction)
+                      .where(Transaction.customer_uuid == customer_uuid)
+                      .where(Transaction.article_uuid == article_uuid)
+                      .first())
+            if not entity:
+                raise InsertError(self.model.__name__, customer_uuid)
+            return entity
+
+    def get_customer_transaction(self, customer_uuid: UUID) -> List[Transaction]:
+        with self.session_factory() as session:
+            entity = session.query(Transaction).where(Transaction.customer_uuid == customer_uuid).all()
+            if not entity:
+                raise NotFoundError(self.model.__name__, customer_uuid)
+            return entity
+
+
 class NotFoundError(Exception):
     def __init__(self, entity_name: str, entity_id: UUID) -> None:
         super().__init__(f"{entity_name} not found, id: {entity_id}")
+
+
+class InsertError(Exception):
+    def __init__(self, entity_name: str, entity_id: UUID) -> None:
+        super().__init__(f"{entity_name} insert error, id: {entity_id}")
