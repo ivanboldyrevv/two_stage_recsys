0ec3bae8cead0a4082a6c04953e43d2b1b8c195a
ivan <1ibah1ivan1@gmail.com>
2025-04-08 13:45:17 +0400

    added recs endpoint

--- backend/src/transport/recs.py
+++ backend/src/transport/recs.py
@@ -1,9 +1,18 @@
-from fastapi import APIRouter
+from uuid import UUID
+from typing import Annotated
+
+from dependency_injector.wiring import Provide, inject
+from services import RecommendationService
+from containers import Container
+
+from fastapi import APIRouter, Depends
 
 
 recs = APIRouter()
 
 
-@recs.get("/recs")
-def get_recommendation():
-    pass
+@recs.get("/two_stage")
+@inject
+def get_recommendation(service: Annotated[RecommendationService, Depends(Provide[Container.recs_service])],
+                       customer_uuid: UUID):
+    return service.get_recommendation(customer_uuid)
